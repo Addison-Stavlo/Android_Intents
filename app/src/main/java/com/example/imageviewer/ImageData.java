f21377db9997064d898762bca1fea29ac3093e45
Addison-Stavlo <AddieStavlo@gmail.com>
2019-06-20 16:21:05 -0400

    updated ImageData to support Uri

--- app/src/main/java/com/example/imageviewer/ImageData.java
+++ app/src/main/java/com/example/imageviewer/ImageData.java
@@ -1,22 +1,24 @@
 package com.example.imageviewer;
 
+import android.net.Uri;
+
 import java.io.Serializable;
 
 public class ImageData implements Serializable {
     private String uri;
     private String name;
 
-    public void setImage(String uri, String name){
-        this.uri = uri;
+    public void setImage(Uri uri, String name){
+        this.uri = uri.toString();
         this.name = name;
     }
 
-    public void setImage(String uri) {
-        this.uri = uri;
+    public void setImage(Uri uri) {
+        this.uri = uri.toString();
     }
-    
-    public String getImage() {
-        return this.uri;
+
+    public Uri getImage() {
+        return Uri.parse(this.uri);
     }
 
     public String getName() {
